83db05f64cf29b67c2ba3c4ffafc89a0bef998b1
a83376750 <394315001@qq.com>
2016-11-12 23:47:45 +0800

    添加mysql可能需要使用的函数

--- MySqlAPI/inc/Syna_Mysql.h
+++ MySqlAPI/inc/Syna_Mysql.h
@@ -1,6 +1,21 @@
 #pragma once
 #include <mysql.h>
-
+/************************************************************************/
+/* mysql_init(MYSQL *mysql)							初始化mysql         */
+/* mysql_real_connect(MYSQL *mysql, const char *host,
+					   const char *user,
+					   const char *passwd,
+					   const char *db,
+					   unsigned int port,
+					   const char *unix_socket,
+					   unsigned long clientflag);	连接数据库           */
+/* mysql_query(MYSQL *mysql, const char *q);		执行数据库命令		*/
+/* 数据库命令 - "SET NAMES GBK" 设置编码									*/
+/* mysql_store_result(MYSQL *mysql);				获取query执行后结果	*/
+/* mysql_fetch_row(MYSQL_RES *result);				获取查询结果的一行	*/
+/* mysql_free_result(results);????					释放结果集			*/
+/* mysql_close(mysql);????							关闭连接数据库		*/
+/************************************************************************/
 namespace Syna
 {
 	class Syna_Mysql
